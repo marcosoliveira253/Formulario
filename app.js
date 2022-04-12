62586c0018cae7652939ea6454ce35ed5ec600e8
Marcosoliveira253 <marcosoliveira253.mo@gmail.com>
2022-04-12 13:21:00 -0300

    atualização

---
+++ app.js
@@ -0,0 +1,7 @@
+function input(){
+    var nome = document.getElementById(nome);
+
+    if(nome.value != ""){
+        alert('Obrigado sr(a)' + 'os seus dados foram encaminhados com sucesso');
+    }
+}
